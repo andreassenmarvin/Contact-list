f36300231d6496d77c42bd012831889e0896c7ae
andreassenmarvin <marvinandreassen@gmail.com>
2021-11-07 22:40:00 +0300

    Create contact class

---
+++ contact.py
@@ -0,0 +1,11 @@
+class Contact:
+
+    contact_list = []
+
+    def __init__(self, fist_name, last_name, number, email):
+
+        self.first_name = fist_name
+        self.last_name = last_name
+        self.number = number
+        self.email = email
+        

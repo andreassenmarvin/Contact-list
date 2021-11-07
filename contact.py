71f4fe7a7fe9f4d7327dc6f9b56616f166a45527
andreassenmarvin <marvinandreassen@gmail.com>
2021-11-07 23:37:41 +0300

    Create delete contact test

--- contact.py
+++ contact.py
@@ -9,6 +9,30 @@ class Contact:
         self.number = number
         self.email = email
 
+
     def save_contact(self):
         Contact.contact_list.append(self)
-        
+
+
+    def delete_contact(self):
+        '''
+        delete_contact method deletes a saved contact from the contact_list
+        '''
+
+        Contact.contact_list.remove(self)
+
+
+    @classmethod
+    def find_by_number(cls, number):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+
+        for contact in cls.contact_list:
+            if contact.number == number:
+                return contact
